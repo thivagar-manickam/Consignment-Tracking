39902be982734a70f9b7c2cc371cf2022a3207f5
Thivagar Manickam <thivagar200@gmail.com>
2020-05-09 20:36:21 +0530

    Renamed consignment-details to add-consigment-details

---
+++ client/src/app/components/add-consignment-details/add-consignment-details.component.ts
@@ -0,0 +1,292 @@
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, Validators, FormGroupDirective } from "@angular/forms";
+import { AuthenticationService } from "../../services/authentication/authentication.service";
+import { ConsignmentService } from "../../services/consignment/consignment.service";
+
+import { NgxSpinnerService } from "ngx-spinner";
+import Swal from "sweetalert2";
+
+import {
+  ADD_CONSIGNMENT,
+  NEW_CONSIGNMENT_TITLE,
+  EDIT_CONSIGNMENT_TITLE,
+  USER_ID,
+  TOKEN,
+  IS_ADD_TOKEN,
+  LOGIN_USER_URL,
+  OBJ,
+  CONSIGNMENT_ID,
+  RETRIEVING_DATA,
+  SAVING_CONSIGNMENT_DETAILS,
+} from "../../utils/constants.js";
+
+@Component({
+  selector: "app-consignment-details",
+  templateUrl: "./add-consignment-details.component.html",
+  styleUrls: ["./add-consignment-details.component.scss"],
+})
+export class AddConsignmentDetailsComponent implements OnInit {
+  @ViewChild(FormGroupDirective, null) form;
+
+  screenTitle = "Add New Consignment";
+  errorMessage = "";
+  isNewConsignment = true;
+  userDetails;
+  consignmentDetail;
+  spinnerMessage;
+
+  consignmentNumberForm = this.consignmentNumberFormBuilder.group({
+    consignmentNumber: ["", Validators.required],
+  });
+
+  contractForm = this.consignmentFormBuilder.group({
+    Contract_Number: ["", Validators.required],
+    Contract_Date: [new Date(), Validators.required],
+    Business_Date: [new Date(), Validators.required],
+    Company_Name: ["", Validators.required],
+    Material: ["", Validators.required],
+    Price: [0, Validators.required],
+    Payment_Terms: ["", Validators.required],
+    Advance_To_Be_Paid: [0],
+    Advance_Paid_Date: [],
+    Contract_Quantity_MT: [0],
+    Invoice_Date: [new Date()],
+    Invoice_Number: [""],
+    Invoice_Price: [],
+    Shipped_Quantity_MT: [0, Validators.required],
+    Containers: [0, Validators.required],
+    Container_Numbers: [""],
+    Loading_Port: [""],
+    OB_L_Date: [new Date()],
+    B_L_Number: [""],
+    PSIC_Courier_Details: [""],
+    DHL_Delivery_Date: [""],
+    Shipping_Line: [""],
+    ETA: [],
+    Place_Of_Delivery: [""],
+    Discharge_Date: [new Date()],
+    Sailing_Time: [""],
+  });
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private _authenticationService: AuthenticationService,
+    private _consignmentService: ConsignmentService,
+    private consignmentFormBuilder: FormBuilder,
+    private consignmentNumberFormBuilder: FormBuilder,
+    private spinnerService: NgxSpinnerService
+  ) {}
+
+  /**
+   * This method will get the form
+   * data and send it to the BE
+   * for saving in the database
+   * @param form
+   * @returns void
+   */
+  onFormSubmit = (form) => {
+    if (this.contractForm.dirty && this.contractForm.valid) {
+      let request = {};
+      const obj = form.getRawValue();
+      obj[USER_ID] = this.userDetails.userId;
+      request[TOKEN] = this.userDetails.token;
+      request[OBJ] = obj;
+      let response;
+
+      this.spinnerMessage = SAVING_CONSIGNMENT_DETAILS;
+      this.spinnerService.show();
+      if (this.isNewConsignment) {
+        response = this._consignmentService.onCreateConsignment(
+          request,
+          IS_ADD_TOKEN
+        );
+      } else {
+        request[CONSIGNMENT_ID] = this.consignmentDetail._id;
+        response = this._consignmentService.onUpdateConsignment(
+          request,
+          IS_ADD_TOKEN
+        );
+      }
+
+      response.subscribe((res) => {
+        let responseValue = JSON.parse(JSON.stringify(res));
+        if (responseValue.success && responseValue.statusCode === 200) {
+          this.errorMessage = "";
+          this.showSuccessMessage(responseValue.message);
+        } else if (!responseValue.success && responseValue.statusCode === 401) {
+          this.errorMessage = responseValue.message;
+          setTimeout(() => {
+            this.router.navigateByUrl(LOGIN_USER_URL);
+          }, 2000);
+        } else {
+          this.errorMessage = responseValue.message;
+        }
+        this.spinnerService.hide();
+      });
+    }
+  };
+
+  /**
+   * This method will show the alert
+   * message on successfully saving the
+   * transaction
+   *
+   * @param message
+   * @returns void
+   */
+  showSuccessMessage = (message) => {
+    Swal.fire({
+      text: message,
+      icon: "success",
+      confirmButtonText: "Ok",
+      allowOutsideClick: false,
+    }).then((result) => {
+      if (result.value) {
+        this.errorMessage = "";
+        if (!this.isNewConsignment) {
+          this.form.resetForm({
+            consignmentNumber: "",
+          });
+        }
+        this.resetFormValues();
+      }
+    });
+  };
+
+  /**
+   * This method will get the
+   * consignement details based on the
+   */
+  onRetrieveDetail = (data) => {
+    if (this.consignmentNumberForm.dirty && this.consignmentNumberForm.valid) {
+      let request = {};
+      request[USER_ID] = this.userDetails.userId;
+      request[TOKEN] = this.userDetails.token;
+      const obj = data.getRawValue();
+      obj.consignmentNumber = obj.consignmentNumber.trim();
+      request[OBJ] = obj;
+      this.spinnerMessage = RETRIEVING_DATA;
+      this.spinnerService.show();
+      this._consignmentService
+        .onRetrieveConsignmentDetail(request, IS_ADD_TOKEN)
+        .subscribe((res) => {
+          let responseValue = JSON.parse(JSON.stringify(res));
+          if (responseValue.success && responseValue.statusCode === 200) {
+            if (responseValue.data) {
+              this.errorMessage = "";
+              this.consignmentDetail = responseValue.data;
+              this.setFormValues(this.consignmentDetail);
+            } else {
+              this.errorMessage = responseValue.message;
+            }
+          } else if (
+            !responseValue.success &&
+            responseValue.statusCode === 401
+          ) {
+            this.errorMessage = responseValue.message;
+            setTimeout(() => {
+              this.router.navigateByUrl(LOGIN_USER_URL);
+            }, 2000);
+          } else {
+            this.errorMessage = responseValue.message;
+          }
+          this.spinnerService.hide();
+        });
+    }
+  };
+
+  /**
+   * This method will set the
+   * form values on retrieving the
+   * consignment details
+   *
+   * @param consignmentDetails
+   */
+  setFormValues = (consignmentDetails) => {
+    this.contractForm.setValue({
+      Contract_Number: consignmentDetails["Contract_Number"],
+      Contract_Date: new Date(consignmentDetails["Contract_Date"]),
+      Business_Date: new Date(consignmentDetails["Business_Date"]),
+      Company_Name: consignmentDetails["Company_Name"],
+      Material: consignmentDetails["Material"],
+      Price: consignmentDetails["Price"],
+      Payment_Terms: consignmentDetails["Payment_Terms"],
+      Advance_To_Be_Paid: consignmentDetails["Advance_To_Be_Paid"],
+      Advance_Paid_Date: consignmentDetails["Advance_Paid_Date"],
+      Contract_Quantity_MT: consignmentDetails["Contract_Quantity_MT"],
+      Invoice_Date: new Date(consignmentDetails["Invoice_Date"]),
+      Invoice_Number: consignmentDetails["Invoice_Number"],
+      Invoice_Price: consignmentDetails["Invoice_Price"],
+      Shipped_Quantity_MT: consignmentDetails["Shipped_Quantity_MT"],
+      Containers: consignmentDetails["Containers"],
+      Container_Numbers: consignmentDetails["Container_Numbers"],
+      Loading_Port: consignmentDetails["Loading_Port"],
+      OB_L_Date: new Date(consignmentDetails["OB_L_Date"]),
+      B_L_Number: consignmentDetails["B_L_Number"],
+      PSIC_Courier_Details: consignmentDetails["PSIC_Courier_Details"],
+      DHL_Delivery_Date: consignmentDetails["DHL_Delivery_Date"],
+      Shipping_Line: consignmentDetails["Shipping_Line"],
+      ETA: new Date(consignmentDetails["ETA"]),
+      Place_Of_Delivery: consignmentDetails["Place_Of_Delivery"],
+      Discharge_Date: new Date(consignmentDetails["Discharge_Date"]),
+      Sailing_Time: consignmentDetails["Sailing_Time"],
+    });
+  };
+
+  /**
+   * This method will reset the form value
+   * on submitting the form
+   *
+   * @returns void
+   */
+  resetFormValues = () => {
+    this.contractForm.reset({
+      Contract_Number: "",
+      Contract_Date: new Date(),
+      Business_Date: new Date(),
+      Company_Name: "",
+      Material: "",
+      Price: 0,
+      Payment_Terms: "",
+      Advance_To_Be_Paid: 0,
+      Advance_Paid_Date: "",
+      Contract_Quantity_MT: 0,
+      Invoice_Date: new Date(),
+      Invoice_Number: "",
+      Invoice_Price: "",
+      Shipped_Quantity_MT: 0,
+      Containers: 0,
+      Container_Numbers: "",
+      Loading_Port: "",
+      OB_L_Date: new Date(),
+      B_L_Number: "",
+      PSIC_Courier_Details: "",
+      DHL_Delivery_Date: "",
+      Shipping_Line: "",
+      ETA: "",
+      Place_Of_Delivery: "",
+      Discharge_Date: new Date(),
+      Sailing_Time: "",
+    });
+    Object.keys(this.contractForm.controls).forEach((key) => {
+      this.contractForm.controls[key].setErrors(null);
+    });
+  };
+
+  ngOnInit() {
+    if (!this._authenticationService.getUserDetail().isUserAuthenticated) {
+      this.router.navigateByUrl(LOGIN_USER_URL);
+    } else {
+      if (this.activatedRoute.snapshot.routeConfig.path === ADD_CONSIGNMENT) {
+        this.isNewConsignment = true;
+        this.screenTitle = NEW_CONSIGNMENT_TITLE;
+      } else {
+        this.isNewConsignment = false;
+        this.screenTitle = EDIT_CONSIGNMENT_TITLE;
+      }
+      this.userDetails = this._authenticationService.getUserDetail();
+    }
+  }
+}
